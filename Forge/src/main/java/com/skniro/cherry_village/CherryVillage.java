33c74edacb076d5f0ac5d136ed5bfa91f1e6afe8
skniro <46392259+skniro@users.noreply.github.com>
2026-03-28 14:05:25 +0800

    26.1

--- Forge/src/main/java/com/skniro/cherry_village/CherryVillage.java
+++ Forge/src/main/java/com/skniro/cherry_village/CherryVillage.java
@@ -1,8 +1,6 @@
 package com.skniro.cherry_village;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -16,10 +14,8 @@ public class CherryVillage {
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
-    public CherryVillage() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        MinecraftForge.EVENT_BUS.register(this);
+    public CherryVillage(FMLJavaModLoadingContext context) {
+        var modEventBus = context.getModBusGroup();
     }
 
 }
